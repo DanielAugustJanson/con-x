166d7af714bb622003461b251ec84ef4144b3ad2
Daniel Janson <70939410+DanielAugustJanson@users.noreply.github.com>
2023-05-10 14:33:19 +0300

    S

--- src/components/GameManager.js
+++ src/components/GameManager.js
@@ -22,9 +22,50 @@ class GameMangager{
             3: "draw"
         }
 
+        /* Sets maximum value for the amount of turns game can have, and keeps in track of how many turns have been taken */
+        this.maxMoves = height*width;
+        this.move = 0;
+
         this.gameState = this.gameStates[0]
         this.turn = this.turns[0]
+        this.outcome = this.outcomes[0]
+    }
 
+    setGameState(state){
+        this.gameState = this.gameStates[state]
+    }
+    setGameOutcome(state){
+        this.outCome = this.outcomes[state]
+    }
+
+    /* Switches between player 1 and 2, sets to locked by default if no value is given */ 
+    swithcPlayerTurn(){
+        switch(this.turn){
+            case(1):
+                this.turn = this.turns[2]
+                break;
+            case(2):
+                this.turn = this.turns[1]
+                break;
+            default:
+                this.turn = this.turns[0]
+        }
+    }
+
+    createGameArea(){
+        /* creates the game are based on values
+        
+        * check that game area is valid compared to the amount of connects neccesary
+        
+        * Sets game state to Ongoing
+
+        * Sets turn for player
+        
+        */
+    }
+
+    checkWinConditions(){
+        /* Checks for the game */
     }
 
 }
