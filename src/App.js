ef3660331e14e8bebe5786dfd36ba3bc5bdc5ff5
Daniel Janson <70939410+DanielAugustJanson@users.noreply.github.com>
2023-05-15 11:24:58 +0300

    LayoutGen

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
+import Test from './components/Test';
+
+
 
 function App() {
   return (
+    <>
+    <div>
+      <Test/>
+    </div>
     <div className="App">
       <p>S</p>
     </div>
+    </>
   );
 }
 
--- src/components/LayoutGenerator.js
+++ src/components/LayoutGenerator.js
@@ -1,14 +1,32 @@
+import "./Layout.css"
+
 class Layout{
     constructor(height,width,connections){
         this.width = width
         this.height = height
-        this.connections = this.connections
+        //this.connections = this.connections
     }
 
-    generateLayout(){
-        
-    }
+    generateLayout() {
+        const grid = [];
+    
+        for (let row = 0; row < this.height; row++) {
+          const rowCells = [];
+    
+          for (let col = 0; col < this.width; col++) {
+            rowCells.push(<div className="grid-cell" key={`${row}-${col}`}></div>);
+          }
+          grid.push(
+            <div className="grid-row" key={row}>
+              {rowCells}
+            </div>
+          );
+        }
+        return <div className="grid">{grid}</div>;
+      }
 
     
 
 }
+
+export default Layout;
